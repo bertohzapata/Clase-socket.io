590803618b66541afb4129fdf11cc780c893ff8c
Humberto Zapata <bertohzapata@HumbertoZapata.local>
2019-10-21 21:28:31 -0500

    Carga a Github

---
+++ README.md
@@ -0,0 +1,7 @@
+# Repositorio de clases de socket.io
+
+## Instalación
+
+Ejecuta el siguiente comando el una terminar ubicada en la carpeta
+
+ ``` npm install  ```
---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+// Inicializar proyecto
+const app = express();
+let server = http.createServer(app);
+
+//Configuracion
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.static(publicPath));
+
+//Iniciando socket.io - comunicación del backend
+let io = socketIO(server);
+
+io.on('connect', (client) => {
+    
+    console.log('Usuario conectado');
+
+    client.on('disconnect', ()=> {
+        console.log('Usuario desconectado');
+    });
+
+    client.on('enviarMensaje', (mensaje) => {
+        console.log(mensaje);
+    });
+
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido al sistema'
+    });
+});
+
+
+server.listen(port, (err) => {
+    if (err) throw new Error(err);
+
+    console.log('Servidor corriendo en el puerto ' + port);
+});
